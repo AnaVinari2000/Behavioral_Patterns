96e5d25555e35b5826ad3a16d2e21760411d939b
Rendrum <iuragutan2000@mail.ru>
2022-05-10 23:53:50 +0300

    adding Obersver Pattern

---
+++ Observer Pattern/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observer_Pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Subject subject = new Subject();
+
+            Observer observer1 = new Observer("Observer 1");
+            subject.Subscribe(observer1);
+
+            Observer observer2 = new Observer("Observer 2");
+            subject.Subscribe(observer2);
+
+            subject.Inventory++;
+
+            Observer observer3 = new Observer("Observer 3");
+            subject.Subscribe(observer3);
+
+            subject.Inventory++;
+
+            Console.ReadLine();
+        }
+    }
+}
